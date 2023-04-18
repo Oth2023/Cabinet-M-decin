b4021a827a14adca2cc71bb1565b515ca5dbe5d3
Oth2023 <130872602+Oth2023@users.noreply.github.com>
2023-04-18 18:57:37 +0200

    assd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,8 +102,6 @@
                             <a href="{{route('produits.index')}}" class="dropdown-item">Liste  Produit</a>
                             <a href="{{route('produits.create')}}" class="dropdown-item">Ajouter  Produit</a>
                             <a href="{{route('produits.create')}}" class="dropdown-item">Ajouter  Produit</a>
-                            <a href="{{route('produits.create')}}" class="dropdown-item">Ajouter  Produit</a>
-                        vvb
                         </div>
                     </div>
                     <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
